e6a888534a4756b1aa1ffc44bed9d18c82178e0d
eddex <eddex@outlook.com>
2019-06-07 17:13:54 +0200

    export mAP data ordered by date

--- calculate_map.py
+++ calculate_map.py
@@ -5,6 +5,8 @@ import os
 def calculate_map():
     os.chdir('darknet/')
     weights_files = glob.glob('backup/*000.weights')
+    weights_files.sort(key=os.path.getmtime)
+    print(weights_files)
     for path in weights_files:
         path_without_extension = path.split('.')[0]
         command = "./darknet detector map signals.data yolov3-tiny-signals.cfg {} > {}.map".format(path, path_without_extension)
